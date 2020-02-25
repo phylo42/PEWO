57c3c3ecd406e36a15ce728cec0ab6bd49a1970d
Nikolai Romashchenko <nikolay.romashchenko@gmail.com>
2020-02-25 13:50:13 +0100

    Travis: fixing conda installation

--- travis/install_conda.sh
+++ travis/install_conda.sh
@@ -20,11 +20,11 @@ else
     sudo apt-get update
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     bash miniconda.sh -b -p $CONDA_DIR -u
-    export PATH="$CONDA_DIR/bin:$PATH"
+    source "$CONDA_DIR/etc/profile.d/conda.sh"
     hash -r
+
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
-
     # Useful for debugging any issues with conda
     conda info -a
 
@@ -35,4 +35,5 @@ else
 
     # Create the environment. Takes time
     conda env create -f "$TRAVIS_BUILD_DIR"/envs/environment.yaml
+    conda activate PEWO
 fi
